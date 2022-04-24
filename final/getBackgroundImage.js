504f0f7fb2ec2c33e54a745044f016dc83c144fa
raco09 <100471045+raco09@users.noreply.github.com>
2022-04-24 17:51:36 +0900

    please...
    please...

--- final/getBackgroundImage.js
+++ final/getBackgroundImage.js
@@ -3,5 +3,5 @@ const bgImage = [ '1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png
 
 
 const chosenImg = bgImage[Math.floor(Math.random()*bgImage.length)];
-body.background = `../img/${chosenImg}`
+body.background = `${chosenImg}`
 
